aec0ffcea89670c19efad64fb25921ae87f9bb45
xlmnope <lmgiroud@gmail.com>
2018-12-12 23:20:12 -0800

    x button added to each li

--- assets/js.js
+++ assets/js.js
@@ -1,3 +1,5 @@
+
+
 // add button
 function newElement() {
   //create li
@@ -11,6 +13,23 @@ function newElement() {
     document.getElementById("list").appendChild(li); // Append <li> to <ul> with id list
   }
   document.getElementById("textinput").value = "" //clears search text input
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
 }
 
+
+
 //close button
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
